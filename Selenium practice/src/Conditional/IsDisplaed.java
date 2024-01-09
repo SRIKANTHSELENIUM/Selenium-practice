be52915f67c0684b76104d2be91a484e6f17493d
SRIKANTH <SRIKANTH@DESKTOP-H5ETRFT>
2024-01-09 19:03:48 +0530

    updated gmail to yahoo

--- Selenium practice/src/Conditional/IsDisplaed.java
+++ Selenium practice/src/Conditional/IsDisplaed.java
@@ -17,7 +17,7 @@ public class IsDisplaed {
 		WebElement email=driver.findElement(By.cssSelector("input[placeholder=\"Enter your work email id\"]"));//cssselector
 		if(email.isDisplayed() && email.isEnabled())
 		{
-			email.sendKeys("srikanth.selenium91@yahoo.com");
+			email.sendKeys("srikanth.selenium91@gmail.com");
 		}
 		driver.findElement(By.xpath("//span[text()=\"CONTINUE\"]")).click();
 		
